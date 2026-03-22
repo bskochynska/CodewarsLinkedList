cb97c27043a8dde776806a7775df044d89021514
Bohdana Skochynska <bohdanaskochynska@gmail.com>
2026-03-22 22:52:44 +0200

    feat: Add documentation for linked list parsing function

--- Parse_a_linked_list_from_a_string/code.py
+++ Parse_a_linked_list_from_a_string/code.py
@@ -4,6 +4,9 @@ module to parse a linked list from a string
 from node import Node
 
 def linked_list_from_string(list_repr: str) -> Node | None:
+    """
+    parses a linked list from a string
+    """
     values = list_repr.split(" -> ")[:-1]
     probe = None
     for i in reversed(values):
